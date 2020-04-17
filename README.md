47ac0424f9eab7efd321c1b6ecbfc83dd7bfd69b
robin2309 <robin.besson1@gmail.com>
2020-04-18 00:01:42 +0200

    intialize react app

--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# Tiendeo Stores Webapp
-
 ## Start up local environment for dev
+
 ```
 $ npm install
 $ npm run start:dev
-```
-
-## Start up docker 
-```
-$ ./run-docker.sh
 ```
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,119 +1,85 @@
-const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const nodeExternals = require('webpack-node-externals');
-const defineVars = require('./define');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const nodeExternals = require("webpack-node-externals");
+const defineVars = require("./define");
 
 const BABEL_LOADER_PLUGINS = [
-  require.resolve('@babel/plugin-proposal-class-properties'),
-  require.resolve('@babel/plugin-proposal-object-rest-spread'),
-  require.resolve('@babel/plugin-transform-runtime')
+  require.resolve("@babel/plugin-proposal-class-properties"),
+  require.resolve("@babel/plugin-proposal-object-rest-spread"),
+  require.resolve("@babel/plugin-transform-runtime")
 ];
 
 const jsLoader = {
-    test: /\.js$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/,
-    options: {
-        presets: ['@babel/preset-env'],
-        plugins: BABEL_LOADER_PLUGINS
-    }
+  test: /\.js$/,
+  loader: "babel-loader",
+  exclude: /node_modules/,
+  options: {
+    presets: ["@babel/preset-env"],
+    plugins: BABEL_LOADER_PLUGINS
+  }
 };
 
 const jsxLoader = {
-    test: /\.jsx$/,
-    loader: 'babel-loader',
-    exclude: /node_modules/,
-    options: {
-        presets: ['@babel/preset-env', '@babel/preset-react'],
-        plugins: BABEL_LOADER_PLUGINS
-    }
+  test: /\.jsx$/,
+  loader: "babel-loader",
+  exclude: /node_modules/,
+  options: {
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: BABEL_LOADER_PLUGINS
+  }
 };
 
 const cssLoader = {
-	test: /\.css$/,
-	use: [
-		MiniCssExtractPlugin.loader,
-		'css-loader'
-	]
+  test: /\.css$/,
+  use: [MiniCssExtractPlugin.loader, "css-loader"]
 };
 
 const lessExtractLoader = {
   test: /\.less$/,
-  use: [
-    MiniCssExtractPlugin.loader,
-    'css-loader', 
-    'less-loader'
-  ]
+  use: [MiniCssExtractPlugin.loader, "css-loader", "less-loader"]
 };
 
 const outputTemplate = {
-	path: path.join(__dirname, "../build"),
-	filename: "[name].bundle.js"
+  publicPath: "/static",
+  path: path.join(__dirname, "../build"),
+  filename: "[name].bundle.js"
 };
 
 const alias = {
-  Common: path.resolve(__dirname, '../src/common/'),
-	Components: path.resolve(__dirname, '../src/common/components/'),
-	Containers: path.resolve(__dirname, '../src/common/containers/'),
-	Client: path.resolve(__dirname, '../src/client/'),
-	Server: path.resolve(__dirname, '../src/server/'),
-	Configs: path.resolve(__dirname, '../src/configs'),
-	Utils: path.resolve(__dirname, '../src/utils/'),
-	Reducers: path.resolve(__dirname, '../src/common/reducers/'),
-  Actions: path.resolve(__dirname, '../src/common/actions/'),
-  Layouts: path.resolve(__dirname, '../src/common/layouts/')
-};
-
-const serverConfig = {
-	target: 'node',
-	mode: 'production',
-	entry: {
-		server: './src/server/index.js'
-  },
-  externals: [nodeExternals()],
-	output: outputTemplate,
-	module: {
-		rules: [
-			jsxLoader,
-			jsLoader
-		]
-  },
-  plugins: [
-    new webpack.DefinePlugin(defineVars.prod.server)
-  ],
-	resolve: {
-		alias,
-		extensions: ['.js', '.jsx']
-	}
+  Common: path.resolve(__dirname, "../src/common/"),
+  Components: path.resolve(__dirname, "../src/common/components/"),
+  Containers: path.resolve(__dirname, "../src/common/containers/"),
+  Client: path.resolve(__dirname, "../src/client/"),
+  Server: path.resolve(__dirname, "../src/server/"),
+  Configs: path.resolve(__dirname, "../src/configs"),
+  Utils: path.resolve(__dirname, "../src/utils/"),
+  Reducers: path.resolve(__dirname, "../src/common/reducers/"),
+  Actions: path.resolve(__dirname, "../src/common/actions/"),
+  Layouts: path.resolve(__dirname, "../src/common/layouts/")
 };
 
 const clientConfig = {
-	target: 'web',
-	mode: 'production',
-	entry: {
-    client: './src/client/index.js'
-	},
-	output: outputTemplate,
-	module: {
-		rules: [
-			jsxLoader,
-			jsLoader,
-			cssLoader,
-			lessExtractLoader
-		]
-	},
-	resolve: {
-		alias,
-		extensions: ['.js', '.jsx', '.css', '.less']
+  target: "web",
+  mode: "production",
+  entry: {
+    client: "./src/index.js"
+  },
+  output: outputTemplate,
+  module: {
+    rules: [jsxLoader, jsLoader, cssLoader, lessExtractLoader]
+  },
+  resolve: {
+    alias,
+    extensions: [".js", ".jsx", ".css", ".less"]
   },
   plugins: [
     new webpack.DefinePlugin(defineVars.prod.client),
     new MiniCssExtractPlugin({
-      filename: 'styles.css'
+      filename: "styles.css"
     })
   ],
-	devtool: 'source-map'
+  devtool: "source-map"
 };
 
-module.exports = [serverConfig, clientConfig];
+module.exports = clientConfig;
